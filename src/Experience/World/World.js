736b3b490155599af6278697b87a6da3a6b1a7fb
Ankur Gurjar <76687403+8ankur8@users.noreply.github.com>
2022-09-22 16:41:52 +0530

    new

---
+++ src/Experience/World/World.js
@@ -0,0 +1,30 @@
+import Experience from '../Experience.js'
+import Environment from './Environment.js'
+import RamenShop from './RamenShop.js'
+import PizzaShop from './PizzaShop.js'
+
+
+export default class World
+{
+    constructor()
+    {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+
+        // Wait for resources
+        this.resources.on('ready', () =>
+        {
+            // Setup
+            //this.ramenShop = new RamenShop()
+            this.pizzaShop = new PizzaShop()
+            //this.hologram = new Hologram()
+            //this.reflections = new Reflections()
+        })
+    }
+
+    update()
+    {
+        if(this.hologram) {this.hologram.update()}
+    }
+}
