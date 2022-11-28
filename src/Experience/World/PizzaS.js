224f311ae98704f0b5b956ef05ded8aef29a15c2
ankur <8ankur8@gmail.com>
2022-11-28 22:10:20 +0530

    Update PizzaS.js

--- src/Experience/World/PizzaS.js
+++ src/Experience/World/PizzaS.js
@@ -67,6 +67,8 @@ export default class PizzaS
         this.model.position.z = - 10
         this.model.position.x = 4
 
+        this.model.scale.set(0.01,0.01,0.01)
+
         this.model.rotation.y = Math.PI/2
         this.scene.add(this.model)
            
